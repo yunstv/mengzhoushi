7487dcfbbef16e231d0881fbc4ae57f183e06b37
zy <yunstv@163.com>
2019-10-31 18:37:28 +0800

    feat: up

---
+++ src/views/log/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="log">
+   index222
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'log',
+  data() {
+    return {
+      msg: '',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
